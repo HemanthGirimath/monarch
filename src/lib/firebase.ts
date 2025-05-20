81ccde8b17bfbb2fb004e07fe560ca8018ce94de
hemanth <hemanthgirimath888@gmail.com>
2025-05-20 23:48:59 +0530

    removed keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2EryYfcrhHRMZfneqdb8gaQmelHTh8EY",
-  authDomain: "monarch-500e9.firebaseapp.com",
-  databaseURL: "https://monarch-500e9-default-rtdb.firebaseio.com",
-  projectId: "monarch-500e9",
-  storageBucket: "monarch-500e9.firebasestorage.app",
-  messagingSenderId: "993258417751",
-  appId: "1:993258417751:web:d06874ef41984ead247d12"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
